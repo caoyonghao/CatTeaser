610bf682dd05aead6901be31f0f8743c3c7e78d8
caoyonghao <caoyonghao@caoyonghaodeMacBook-Air.local>
2016-09-25 02:47:02 +0800

    test require

--- driver/servo.js
+++ driver/servo.js
@@ -1,18 +1,20 @@
 'use strict';
 
-var Gpio = require('../').Gpio,
-  motor = new Gpio(10, {mode: Gpio.OUTPUT}),
-  pulseWidth = 1000,
-  increment = 100;
+const Gpio = require('pigpio').Gpio;
+const motor =  new Gpio(10, {mode: Gpio.OUTPUT});
+// var Gpio = require('../').Gpio,
+//   motor = new Gpio(10, {mode: Gpio.OUTPUT}),
+//   pulseWidth = 1000,
+//   increment = 100;
 
-setInterval(function () {
-  motor.servoWrite(pulseWidth);
+// setInterval(function () {
+//   motor.servoWrite(pulseWidth);
 
-  pulseWidth += increment;
-  if (pulseWidth >= 2000) {
-    increment = -100;
-  } else if (pulseWidth <= 1000) {
-    increment = 100;
-  }
-}, 1000);
+//   pulseWidth += increment;
+//   if (pulseWidth >= 2000) {
+//     increment = -100;
+//   } else if (pulseWidth <= 1000) {
+//     increment = 100;
+//   }
+// }, 1000);
 
