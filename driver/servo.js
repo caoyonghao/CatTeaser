ea0c29a66adb8f61da6b19353637d7e6ede5fdb9
caoyonghao <caoyonghao@caoyonghaodeMacBook-Air.local>
2016-09-25 18:53:25 +0800

    test

--- driver/servo.js
+++ driver/servo.js
@@ -24,7 +24,7 @@ class moto {
 
   }
 }
-exports.moto = moto;
+module.exports.moto = moto;
 // var Gpio = require('../').Gpio,
 //   motor = new Gpio(10, {mode: Gpio.OUTPUT}),
 //   pulseWidth = 1000,
